ee860a221cb5fc6e0fc89b42102188cabb72e1be
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-04-18 21:17:28 +0200

    Updates

---
+++ precache-manifest.5a4693f0222c44b7af3db2fdf2e8c92b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b469847b7b4a707b5e0709ea11dc5427",
+    "url": "/memory-stretcher/index.html"
+  },
+  {
+    "revision": "7ae9f34ed748c32db4ba",
+    "url": "/memory-stretcher/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "239d35ee7ead4bfcb0ae",
+    "url": "/memory-stretcher/static/css/main.68a5b254.chunk.css"
+  },
+  {
+    "revision": "7ae9f34ed748c32db4ba",
+    "url": "/memory-stretcher/static/js/2.20664234.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/memory-stretcher/static/js/2.20664234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "239d35ee7ead4bfcb0ae",
+    "url": "/memory-stretcher/static/js/main.0e5db058.chunk.js"
+  },
+  {
+    "revision": "6d76702208bd0b8b6bd1",
+    "url": "/memory-stretcher/static/js/runtime-main.96ed6a1c.js"
+  }
+]);
