c65a16d5ebbe15d6dbc634fe568227d4658e8461
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-04-21 23:23:37 +0200

    updated instruction

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ function App() {
               Randomly chosen nodes are displayed for short amount of time.{" "}
             </h4>
             <h4>
-              Player's task is to memorized and find them all in a less than
+              Player's task is to memorize and find them all in a less than
               three attempts.
             </h4>
           </header>
