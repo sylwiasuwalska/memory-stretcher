b493edda9254abb4fc4f21f73b05ae5a3027b9cc
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-05-01 20:27:26 +0200

    testing

--- src/tests/Options.test.js
+++ src/tests/Options.test.js
@@ -24,7 +24,7 @@ test('check if modal is shown after click button OPTIONS', () => {
     expect(modalText).toBeInTheDocument;
 });
 
-test('time option invokes function changing time',  (done) => {
+test('time options invokes function changing time',  (done) => {
     const handleClick = () => {
         done();
     }
@@ -38,7 +38,7 @@ test('time option invokes function changing time',  (done) => {
     fireEvent.click(radioOption);
 });
 
-test('level option invokes function changing level',  (done) => {
+test('level options invokes function changing level',  (done) => {
     const handleClick = () => {
         done();
     }
@@ -66,5 +66,22 @@ test('click on save button hides modal',  () => {
     const saveButton = getByText("SAVE")
     expect(saveButton).toBeInTheDocument;
     fireEvent.click(saveButton);
+    expect(modalText).not.toBeInTheDocument;
+});
+
+test('click on close button hides modal',  () => {
+    const { getByText } = render(<Options setDisplayTime
+                                          setHowManyNodes
+                                          arraySize={5}/>);
+
+    const buttonOptions = getByText("OPTIONS")
+
+    fireEvent.click(buttonOptions);
+    const modalText = getByText("Set display time:")
+    expect(modalText).toBeInTheDocument;
+
+    const closeButton = getByText("×")
+    expect(closeButton).toBeInTheDocument;
+    fireEvent.click(closeButton);
     expect(modalText).not.toBeInTheDocument;
 });
